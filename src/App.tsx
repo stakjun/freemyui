b1921066c79c4374dbb25e1eabd8ab8b8b1cbe15
junjie <578715385@qq.com>
2023-03-08 22:48:12 +0800

    feat: Button组件完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
+import Button, { ButtonSize, ButtonType } from './components/Button/button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button className='yes'>hello</Button>
+      <Button autoFocus onClick={() => alert(1)} btnType={ButtonType.Primary} size={ButtonSize.Small} >hello</Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Large}>hello</Button>
+      <Button btnType={ButtonType.Link}  href='http://www.baidu.com' target="_blank">baidu</Button>
+      <Button btnType={ButtonType.Link}  href='http:www.baidu.com'>baidu</Button>
     </div>
   );
 }
