cf06d5a7c38f96c0b44e1534b6a5b0de09445136
junjie <578715385@qq.com>
2023-03-09 09:39:03 +0800

    feat: Alert组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Alert, { AlertType } from './components/Alert/alert';
 import Button, { ButtonSize, ButtonType } from './components/Button/button';
 
 function App() {
@@ -9,6 +10,30 @@ function App() {
       <Button btnType={ButtonType.Danger} size={ButtonSize.Large}>hello</Button>
       <Button btnType={ButtonType.Link}  href='http://www.baidu.com' target="_blank">baidu</Button>
       <Button btnType={ButtonType.Link}  href='http:www.baidu.com'>baidu</Button>
+
+      <Alert 
+        title='123'
+        type={AlertType.Default}
+        className='myAlert'
+      />
+       <Alert 
+        title='123'
+        type={AlertType.Warning}
+        className='myAlert'
+      />
+      <Alert 
+        title='123'
+        type={AlertType.Success}
+        className='myAlert'
+        description='hah '
+      />
+      <Alert 
+        title='124'
+        type={AlertType.Danger}
+        description='我的'
+        closable={true}
+        onClose={() => {console.log(1)}}
+      />
     </div>
   );
 }
